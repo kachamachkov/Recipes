d77be6667e4601c2ba7a22975c9ae7a4f55b32d7
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-28 12:20:26 +0300

    Update & test main route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,8 +10,11 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/api/recipes/search', async (req, res) => {
-    res.json({ message: 'success' });
+    const searchterm = req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
 
+    const results = await RecipeAPI.searchRecipes(searchterm, page);
+    return res.json(results);
 });
 
 app.listen(5000, () => {
