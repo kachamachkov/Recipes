a98c2fac6e86de7fd2277c831aeaef4d0e36df01
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-28 14:20:25 +0300

    Render test recipes at frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,37 @@
-import './App.css'
+import { FormEvent, useState } from 'react';
+import './App.css';
+import * as api from './api';
+import { Recipe } from './types';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('burgers');
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+
+      setRecipes(recipes.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <div>Hello from recipe app!</div>
+    <div>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <button type='submit'>Submit</button>
+      </form>
+
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          Recipe image location: {recipe.image}
+          Recipe title: {recipe.title}
+
+        </div>
+      ))}
+    </div>
   );
 };
 
