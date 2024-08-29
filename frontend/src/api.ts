8b6333fef2226e8c5a7c876a85b16182480f132e
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-29 09:47:58 +0300

    Add/get favorite recipes to api ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,5 @@
+import { Recipe } from "./types";
+
 export const searchRecipes = async (searchTerm: string, page: number) => {
     const baseUrl = new URL('http://localhost:5000/api/recipes/search');
     baseUrl.searchParams.append('searchTerm', searchTerm);
@@ -21,4 +23,34 @@ export const getRecipeSummary = async (recipeId: string) => {
     }
 
     return response.json();
+};
+
+export const getFavoriteRecipes = async () => {
+    const url = new URL(`http://localhost:5000/api/recipes/favorite`);
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response.json();
+};
+
+export const addFavoriteRecipe = async (recipe: Recipe) => {
+    const url = new URL(`http://localhost:5000/api/recipes/favorite`);
+    const body = {
+        recipeId: recipe.id
+    };
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 };
