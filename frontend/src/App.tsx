17574700b603c7f264940e7856c5044b0c8d6c28
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-29 09:47:14 +0300

    Add favorite recipe functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
-import { FormEvent, useRef, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
 import * as api from './api';
 import { Recipe } from './types';
 import RecipeCard from './components/RecipeCard';
 import RecipeModal from './components/RecipeModal';
 
+type Tabs = 'search' | 'favorites';
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(undefined);
+  const [selectedTab, setSelectedTab] = useState<Tabs>('search');
+  const [favoriteRecipes, setFavoriteRecipes] = useState<Recipe[]>([]);
   const pageNumber = useRef(1);
+
+  useEffect(() => {
+    const fetchFavoriteRecipes = async () => {
+      try {
+        const favoriteRecipes = await api.getFavoriteRecipes();
+        setFavoriteRecipes(favoriteRecipes.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchFavoriteRecipes();
+
+  }, []);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -35,27 +53,66 @@ const App = () => {
     }
   };
 
+  const addFavoriteRecipe = async (recipe: Recipe) => {
+    try {
+
+      await api.addFavoriteRecipe(recipe);
+      setFavoriteRecipes([...favoriteRecipes, recipe]);
+
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <form onSubmit={(event) => handleSearchSubmit(event)}>
-        <input
-          type="text"
-          required
-          placeholder='Enter a search term ...'
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        />
-        <button type='submit'>Submit</button>
-      </form>
+      <div className='tabs'>
+        <h1 onClick={() => setSelectedTab('search')}> Recipe Search</h1>
+        <h1 onClick={() => setSelectedTab('favorites')}> Favorites </h1>
+      </div>
+      {selectedTab === 'search' && (
+        <>
+          <form onSubmit={(event) => handleSearchSubmit(event)}>
+            <input
+              type="text"
+              required
+              placeholder='Enter a search term ...'
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button type='submit'>Submit</button>
+          </form>
 
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
-      ))}
+          {recipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              onClick={() => setSelectedRecipe(recipe)}
+              onFavoriteButtonClick={addFavoriteRecipe}
+            />
+          ))}
 
-      <button
-        className='view-more-button'
-        onClick={handleViewMore}
-      >View More</button>
+          <button
+            className='view-more-button'
+            onClick={handleViewMore}
+          >View More</button>
+        </>)}
+
+      {selectedTab === 'favorites' && (
+        <div>
+          {favoriteRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              onClick={() => setSelectedRecipe(recipe)}
+              onFavoriteButtonClick={() => undefined}
+            />
+          ))}
+
+
+        </div>
+      )}
 
       {selectedRecipe ? <RecipeModal recipeId={selectedRecipe.id.toString()} onClose={() => setSelectedRecipe(undefined)} /> : null}
     </div>
